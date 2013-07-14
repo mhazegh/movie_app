d8982ba70fb5b6731fa69685626a88409f9d7302
mhazegh <mhazegh@gmail.com>
2013-07-14 01:41:03 -0600

    v1.0 complete

--- web.js
+++ web.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
 
+app.use(express.static(__dirname+'/public'));
+
+app.configure(function(){
+  app.use('/', express.static(__dirname + '/'));
+});
+
 app.get('/', function(request, response) {
   var buf = fs.readFileSync('index.html');
   response.send(buf.toString());
